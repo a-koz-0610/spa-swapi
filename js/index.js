21b1af0f5a17e7dab7781d2a2e9d73a6a2a55b47
alan <alan@wecancodeit.org>
2021-07-10 10:33:27 -0400

    create Footer component and add to index.js

--- js/index.js
+++ js/index.js
@@ -1,12 +1,19 @@
 import Header from './components/Header.js';
+import Footer from './components/Footer.js';
 
 buildPage();
 
 function buildPage() {
   header();
+  footer();
 }
 
 function header() {
   const headerElement = document.querySelector('.header');
   headerElement.innerHTML = Header();
+}
+
+function footer() {
+  const footerElement = document.querySelector('.footer');
+  footerElement.innerHTML = Footer();
 }
